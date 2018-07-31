e7703831f2f884023d774af1f02e5441cc6adf0b
niulingyun <leandre@qq.com>
2018-07-31 16:08:29 +0800

    update@Tue Jul 31 16:08:29 CST 2018

--- setup.py
+++ setup.py
@@ -5,20 +5,9 @@ from setuptools import (
     find_packages,
 )
 
-
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
-    long_description = f.read()
-
-
 setup(
     name="web3-gear",
     version="1.0.8",
-    description="An adapter between thor-restful and eth-rpc.",
-    long_description=long_description,
-    url="https://github.com/vechain/web3-gear",
-    author="Han Xiao",
-    author_email="smallcpp@foxmail.com",
     license="MIT",
     classifiers=[
         'Intended Audience :: Developers',
